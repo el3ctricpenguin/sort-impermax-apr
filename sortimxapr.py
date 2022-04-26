a85ae05714a5d6e9fbe75fa27ae6fc0583cad060
el3ctricpenguin <86659455+el3ctricpenguin@users.noreply.github.com>
2022-04-26 22:41:43 +0900

    Add files via upload
    まだまだ動かない。https://polygon.impermax.finance の'7 days average'のボタンを押したい。

--- sortimxapr.py
+++ sortimxapr.py
@@ -12,7 +12,6 @@ driver.get(url) #seleniumでurlのページを開く
 time.sleep(5)
 print("!")
 
-#seleniumで扱いたいものをBSで選択してるのがおかしそう
 
 #soupにhtmlを読み込ませる
 soup = BeautifulSoup(driver.page_source,'html.parser')
@@ -20,7 +19,7 @@ results = soup.find_all(class_="title")
 
 #特定の文字列を含む要素をクリックしたかった。くやしいが動かん！
 #driver.find_element(By.XPATH,"//*[contains(@text,'24 hours average')]").click()
-driver.find_element(By.XPATH,'//*[@id="impermax-app"]/div/div/div/div[1]/div[3]/div/div[2]/div/div[2]/span').click()
+driver.find_element(By.XPATH,'//div[@class="ml-2"]/div[2]/span').click()
 time.sleep(1)
 table = soup.select("div.card-body > .pairs-table")
 row = soup.select("#impermax-app div.home > div.mt-2.container   > a.pairs-table-row:nth-child(2)")
